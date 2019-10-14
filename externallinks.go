82bbbed03a6e46734621c2a023f148979b2cde18
Mike McMahon <mike.mcmahon@wavefront.com>
2019-10-14 15:23:15 -0700

    Fleshed out externa links

--- externallinks.go
+++ externallinks.go
@@ -3,6 +3,7 @@ package wavefront
 import (
 	"encoding/json"
 	"fmt"
+	"io/ioutil"
 )
 
 type ExternalLink struct {
@@ -18,6 +19,8 @@ type ExternalLink struct {
 	SourceFilterRegex     string            `json:"SourceFilterRegex,omitempty"`
 	PointTagFilterRegexes map[string]string `json:"PointTagFilterRegexes,omitempty"`
 }
+
+const baseExtLinkPath = "/api/v2/extlink"
 
 type ExternalLinks struct {
 	client Wavefronter
@@ -60,21 +63,66 @@ func (e ExternalLinks) Get(link *ExternalLink) error {
 	if *link.ID == "" {
 		return fmt.Errorf("id must be specified")
 	}
-	return fmt.Errorf("not yet implemented")
+
+	return e.crudExtLinks("GET", fmt.Sprintf("%s/%s", baseExtLinkPath, *link.ID), link)
 }
 
 func (e ExternalLinks) Create(link *ExternalLink) error {
-	return fmt.Errorf("not yet implemented")
+	if link.Name == "" || link.Description == "" || link.Template == "" {
+		return fmt.Errorf("externa link name, description, and template must be specified")
+	}
+
+	return e.crudExtLinks("POST", baseExtLinkPath, link)
 }
 
 func (e ExternalLinks) Update(link *ExternalLink) error {
 	if *link.ID == "" {
 		return fmt.Errorf("id must be specified")
 	}
-	return fmt.Errorf("not yet implemented")
+
+	return e.crudExtLinks("POST", fmt.Sprintf("%s/%s", baseExtLinkPath, *link.ID), link)
 }
 
 func (e ExternalLinks) Delete(link *ExternalLink) error {
+	if *link.ID == "" {
+		return fmt.Errorf("id must be specified")
+	}
+
+	err := e.crudExtLinks("DELETE", fmt.Sprintf("%s/%s", baseExtLinkPath, *link.ID), link)
+	if err != nil {
+		return err
+	}
+
+	// Clear out the id to prevent re-submission
 	*link.ID = ""
-	return fmt.Errorf("not yet implemented")
+	return nil
+}
+
+func (e ExternalLinks) crudExtLinks(method, path string, extLink *ExternalLink) error {
+	payload, err := json.Marshal(extLink)
+	if err != nil {
+		return err
+	}
+
+	request, err := e.client.NewRequest(method, path, nil, payload)
+	if err != nil {
+		return err
+	}
+
+	resp, err := e.client.Do(request)
+	if err != nil {
+		return err
+	}
+	defer resp.Close()
+
+	body, err := ioutil.ReadAll(resp)
+	if err != nil {
+		return err
+	}
+
+	return json.Unmarshal(body, &struct{
+		Response ExternalLink `json:"response"`
+	}{
+		Response: *extLink,
+	})
 }
